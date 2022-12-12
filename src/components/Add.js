9dab0e0701caaed7bc3ff7443ef0359f92d12bcc
Amit Kumar <akamit388@gmail.com>
2022-12-12 18:02:03 +0530

    add data

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { addbook } from '../redux/book/bookSlice';
 
 const Add = () => {
+  let dispatch = useDispatch();
+  let navigate = useNavigate();
+
+  const [formData, setFormData] = useState({title:'', body:''});
+  const {title, body} = formData;
+
+  const onchangeHandler = (e) => {
+    setFormData({...formData, [e.target.name]:e.target.value})
+  }
+
+  //console.log(formData)
+
+  const formDataHandler = (e) => {
+    e.preventDefault();
+    dispatch(addbook({title,body}));
+    setFormData({title:'', body:''});
+    navigate('/');
+  }
+
   return (
-    <div>
-      <h1>add 12</h1>
-    </div>
+    <>
+      <p className='mt-5'><Link to='/'>Back to home</Link></p>
+      <div className='row justify-content-center'>
+        <div className='col-md-5 card'>
+          <form className='card-body' onSubmit={formDataHandler}>
+            <div className="mb-3">
+              <label>Title</label>
+              <input type="text" className="form-control" name="title" value={title} placeholder="Title" onChange={onchangeHandler} />
+            </div>
+            <div className="mb-3">
+              <label>Body</label>
+              <input type="text" className="form-control" name="body" value={body} placeholder="Body" onChange={onchangeHandler} />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
